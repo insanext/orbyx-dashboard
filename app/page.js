86fe81ca3ecd589419955536e3ee127c190ede87
insanext <camilo.merino.m@gmail.com>
2026-03-03 23:24:58 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Orbyx Dashboard 🚀</h1>
+      <p>Tu dashboard SaaS ya está funcionando en Vercel.</p>
+    </div>
+  );
+}
