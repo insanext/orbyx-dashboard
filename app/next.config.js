55a6edb7865b1f5c8e6b5f7e4947797b90a1a562
insanext <camilo.merino.m@gmail.com>
2026-03-04 21:18:56 -0300

    Create next.config.js

---
+++ app/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; connect-src 'self' https://orbyx-backend.onrender.com; img-src 'self' data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
